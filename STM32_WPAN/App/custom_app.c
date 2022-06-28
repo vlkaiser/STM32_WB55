12186cd81e080e4cb566bb981fae55064c256dda
vlkaiser <vlkaiser9@gmail.com>
2022-06-28 15:33:31 -0400

    first commit - working demo with ST BLE Toolbox, toggles led on 0x22 and reports SW1 presses

---
+++ STM32_WPAN/App/custom_app.c
@@ -0,0 +1,225 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    App/custom_app.c
+  * @author  MCD Application Team
+  * @brief   Custom Example Application (Server)
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "app_common.h"
+#include "dbg_trace.h"
+#include "ble.h"
+#include "custom_app.h"
+#include "custom_stm.h"
+#include "stm32_seq.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+typedef struct
+{
+  /* LED_SVC */
+  uint8_t               Mycharnotify_Notification_Status;
+  /* USER CODE BEGIN CUSTOM_APP_Context_t */
+
+  /* USER CODE END CUSTOM_APP_Context_t */
+
+  uint16_t              ConnectionHandle;
+} Custom_App_Context_t;
+
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private defines ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macros -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/**
+ * START of Section BLE_APP_CONTEXT
+ */
+
+PLACE_IN_SECTION("BLE_APP_CONTEXT") static Custom_App_Context_t Custom_App_Context;
+
+/**
+ * END of Section BLE_APP_CONTEXT
+ */
+
+/* USER CODE BEGIN PV */
+uint8_t UpdateCharData[247];
+uint8_t NotifyCharData[247];
+
+uint8_t SecureReadData;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+  /* LED_SVC */
+static void Custom_Mycharnotify_Update_Char(void);
+static void Custom_Mycharnotify_Send_Notification(void);
+
+/* USER CODE BEGIN PFP */
+	void myTask(void)
+	{
+		//Read the Button State
+		if(!HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin))
+		{
+			UpdateCharData[0] ^= 0x1;
+			Custom_Mycharnotify_Update_Char();		//Depends on Characteristic long name from CubeMX
+		}
+		UTIL_SEQ_SetTask(1 << CFG_TASK_MY_TASK, CFG_SCH_PRIO_0);
+	}
+
+/* USER CODE END PFP */
+
+/* Functions Definition ------------------------------------------------------*/
+void Custom_STM_App_Notification(Custom_STM_App_Notification_evt_t *pNotification)
+{
+  /* USER CODE BEGIN CUSTOM_STM_App_Notification_1 */
+
+  /* USER CODE END CUSTOM_STM_App_Notification_1 */
+  switch(pNotification->Custom_Evt_Opcode)
+  {
+    /* USER CODE BEGIN CUSTOM_STM_App_Notification_Custom_Evt_Opcode */
+
+    /* USER CODE END CUSTOM_STM_App_Notification_Custom_Evt_Opcode */
+
+  /* LED_SVC */
+    case CUSTOM_STM_MYCHARWRITE_WRITE_EVT:
+      /* USER CODE BEGIN CUSTOM_STM_MYCHARWRITE_WRITE_EVT */
+
+      /* USER CODE END CUSTOM_STM_MYCHARWRITE_WRITE_EVT */
+      break;
+
+    case CUSTOM_STM_MYCHARNOTIFY_NOTIFY_ENABLED_EVT:
+      /* USER CODE BEGIN CUSTOM_STM_MYCHARNOTIFY_NOTIFY_ENABLED_EVT */
+
+      /* USER CODE END CUSTOM_STM_MYCHARNOTIFY_NOTIFY_ENABLED_EVT */
+      break;
+
+    case CUSTOM_STM_MYCHARNOTIFY_NOTIFY_DISABLED_EVT:
+      /* USER CODE BEGIN CUSTOM_STM_MYCHARNOTIFY_NOTIFY_DISABLED_EVT */
+
+      /* USER CODE END CUSTOM_STM_MYCHARNOTIFY_NOTIFY_DISABLED_EVT */
+      break;
+
+    default:
+      /* USER CODE BEGIN CUSTOM_STM_App_Notification_default */
+
+      /* USER CODE END CUSTOM_STM_App_Notification_default */
+      break;
+  }
+  /* USER CODE BEGIN CUSTOM_STM_App_Notification_2 */
+
+  /* USER CODE END CUSTOM_STM_App_Notification_2 */
+  return;
+}
+
+void Custom_APP_Notification(Custom_App_ConnHandle_Not_evt_t *pNotification)
+{
+  /* USER CODE BEGIN CUSTOM_APP_Notification_1 */
+
+  /* USER CODE END CUSTOM_APP_Notification_1 */
+
+  switch(pNotification->Custom_Evt_Opcode)
+  {
+    /* USER CODE BEGIN CUSTOM_APP_Notification_Custom_Evt_Opcode */
+
+    /* USER CODE END P2PS_CUSTOM_Notification_Custom_Evt_Opcode */
+    case CUSTOM_CONN_HANDLE_EVT :
+      /* USER CODE BEGIN CUSTOM_CONN_HANDLE_EVT */
+
+      /* USER CODE END CUSTOM_CONN_HANDLE_EVT */
+      break;
+
+    case CUSTOM_DISCON_HANDLE_EVT :
+      /* USER CODE BEGIN CUSTOM_DISCON_HANDLE_EVT */
+
+      /* USER CODE END CUSTOM_DISCON_HANDLE_EVT */
+      break;
+
+    default:
+      /* USER CODE BEGIN CUSTOM_APP_Notification_default */
+
+      /* USER CODE END CUSTOM_APP_Notification_default */
+      break;
+  }
+
+  /* USER CODE BEGIN CUSTOM_APP_Notification_2 */
+
+  /* USER CODE END CUSTOM_APP_Notification_2 */
+
+  return;
+}
+
+void Custom_APP_Init(void)
+{
+  /* USER CODE BEGIN CUSTOM_APP_Init */
+
+  /* USER CODE END CUSTOM_APP_Init */
+  return;
+}
+
+/* USER CODE BEGIN FD */
+
+/* USER CODE END FD */
+
+/*************************************************************
+ *
+ * LOCAL FUNCTIONS
+ *
+ *************************************************************/
+
+  /* LED_SVC */
+void Custom_Mycharnotify_Update_Char(void) /* Property Read */
+{
+  Custom_STM_App_Update_Char(CUSTOM_STM_MYCHARNOTIFY, (uint8_t *)UpdateCharData);
+  /* USER CODE BEGIN Mycharnotify_UC*/
+
+  /* USER CODE END Mycharnotify_UC*/
+  return;
+}
+
+void Custom_Mycharnotify_Send_Notification(void) /* Property Notification */
+ {
+  if(Custom_App_Context.Mycharnotify_Notification_Status)
+  {
+    Custom_STM_App_Update_Char(CUSTOM_STM_MYCHARNOTIFY, (uint8_t *)NotifyCharData);
+    /* USER CODE BEGIN Mycharnotify_NS*/
+
+    /* USER CODE END Mycharnotify_NS*/
+  }
+  else
+  {
+    APP_DBG_MSG("-- CUSTOM APPLICATION : CAN'T INFORM CLIENT -  NOTIFICATION DISABLED\n ");
+  }
+  return;
+}
+
+/* USER CODE BEGIN FD_LOCAL_FUNCTIONS*/
+
+/* USER CODE END FD_LOCAL_FUNCTIONS*/
